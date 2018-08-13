dcb379ad42153a97bbd7cb5cc07e37c3c3f43608
Patrick Dureza <pgdureza@gmail.com>
2018-08-13 21:03:11 +0800

    moved module styles to scss,

---
+++ src/scripts/home.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  // home video link
+  $('a.home-video-link').on('click', function(e){
+    e.preventDefault();
+    $("#home-video-wrapper").modal({
+      fadeDuration: 300,
+      fadeDelay: 0.5,
+      showClose: true
+    });
+  });
+
+  // slider
+  $("#home-jumbo-slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });     
+
+})
