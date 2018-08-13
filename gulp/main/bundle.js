3fdf95bf2084becff9152f74cc59eb5ae98c24df
Patrick Dureza <pgdureza@gmail.com>
2018-08-13 17:33:48 +0800

    initial commit

---
+++ gulp/main/bundle.js
@@ -0,0 +1,102 @@
+require("../misc/dependencies.js")();
+
+/* -----  Start Tasks for Main Website Styling ----- */
+
+/* Main.css */
+
+gulp.task("clean_bundle", done => {
+  clean([appRoot.path + config.app.paths.dist + config.app.paths.distCSS]);
+  done();
+});
+
+gulp.task("sass_autoprefixer_bundle", done => {
+  run_sass_autoprefixer(
+    [
+      appRoot.path +
+        config.app.paths.styles +
+        "/" +
+        config.app.input.filename.css
+    ], // Source
+    appRoot.path + config.app.paths.dist + config.app.paths.distCSS, // Destination
+    { grid: true }, //Autoprefixer Config
+    { reduceIdents: false } //cssNano Config -- added due to keyframes renaming issue, this prevents that.
+  );
+  done();
+});
+
+gulp.task("pushSytles", done => {
+  if (config.ftp.enabled) {
+    setTimeout(function() {
+      uploadStyles();
+    }, 2000); // Timeout just to be sure everything has finished to be written
+  }
+  done();
+});
+
+function styles_bundle(done) {
+  gulp.series("sass_autoprefixer_bundle", "pushSytles", () => {
+    done();
+  })();
+}
+
+gulp.task("pushScripts", done => {
+  if (config.ftp.enabled) {
+    setTimeout(function() {
+      uploadScripts();
+    }, 500);
+  }
+  done();
+});
+
+gulp.task("obfuscate", done => {
+  obfuscate(
+    appRoot.path + config.app.paths.scripts + "/**/*.js",
+    appRoot.path + config.app.paths.dist + config.app.paths.distJS
+  );
+  done();
+});
+
+gulp.task("uglify", done => {
+  run_uglify(
+    appRoot.path + config.app.paths.scripts + "**/*.js",
+    appRoot.path + config.app.paths.dist + config.app.paths.distJS
+  );
+  done();
+});
+
+function scripts_bundle(done) {
+  gulp.series("uglify", "pushScripts", () => {
+    done();
+  })();
+}
+
+gulp.task("production", () => {
+  gulp.series("obfuscate", "pushScripts", (done) => {
+    done();
+  })();
+});
+
+gulp.task("watch:scripts", () => {
+  gulp.watch(
+    appRoot.path + config.app.paths.scripts + "/**/*.js",
+    scripts_bundle
+  );
+});
+
+gulp.task("watch:styles", () => {
+  gulp.watch(appRoot.path + config.app.paths.styles +
+    gulpif(config.app.input.compiler.styles.sass === true,
+      '**/*.scss',
+      gulpif(config.app.input.compiler.styles.less === true,
+        '**/*.less',
+        gulpif(config.app.input.compiler.styles.less === true,
+          '**/*.styl'
+        )
+      )
+    ),
+    styles_bundle);
+});
+
+gulp.task("watch", gulp.parallel("watch:scripts", "watch:styles"));
+
+gulp.task("default", gulp.parallel("watch"));
---
+++ gulp/misc/utils.js
@@ -0,0 +1,129 @@
+/* ----- Common Functions ----- */
+
+module.exports = function () {
+  this.run_sass_autoprefixer = function (src, destination, autoprefixerConfig, cssnanoConfig) {
+    var plugins = [
+      this.postcssnormalize,
+      this.autoprefixer(autoprefixerConfig),
+      this.cssnano(cssnanoConfig)
+    ]
+    this.pump([
+      this.gulp.src(src),
+      // this.sourcemaps.init(),
+      this.gulpif(this.config.app.input.compiler.styles.sass === true, this.sass().on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      }), this.gulpif(this.config.app.input.compiler.styles.less === true, this.less().on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      }), this.gulpif(this.config.app.input.compiler.styles.less === true, this.stylus().on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      })))),
+      this.postcss(plugins).on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      }),
+      this.rename(this.config.app.output.filename.css),
+      // this.sourcemaps.write('./maps'),
+      this.gulp.dest(destination),
+      this.touch() // For some reason, the output bundle wasn't updating its last modifed date, so add this to make sure if does that.
+    ])
+  }
+  this.run_uglify = function (src, destination) {
+    this.pump([
+      // this.sourcemaps.init(),
+      this.gulpif(this.config.app.input.compiler.scripts.babel === true, this.gulp.src([
+        './node_modules/babel-polyfill/dist/polyfill.min.js',
+        src
+      ]), // includes polyfill for browser compatibility
+      this.babel().on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      })),
+      this.concat(this.config.app.output.filename.scripts),
+      this.strip(),
+      this.gulp.dest(destination),
+      this.gulpif(this.config.app.output.uglify === true, this.uglify().on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      }), this.gulp.dest(destination)),
+      // this.sourcemaps.write('./maps'),
+      this.gulp.dest(destination)
+    ])
+  }
+  this.obfuscate = function (src, destination) {
+    this.pump([
+      this.gulp.src(src),
+      this.concat(this.config.app.output.filename.scripts),
+      this.gulp.dest(destination),
+      this.obfuscator({
+        compact: true,
+        sourceMap: true
+      }).on('error', function (err) {
+        this.gutil.log(this.gutil.colors.red('[Error]'), err.toString())
+        this.emit('end')
+        return false
+      }),
+      this.gulp.dest(destination)
+    ])
+  }
+  function filepathExists (path) {
+    if (this.fs.existsSync(path)) {
+      return true
+    } else {
+      this.gutil.log(this.gutil.colors.red('[Error]'), "File doesn't exists, please verify your paths & files -> " + path)
+      return false
+    }
+  }
+  function push (globs, dest, styles) {
+    var conn = this.ftp.create({
+      host: this.config.ftp.host,
+      user: this.config.ftp.user,
+      password: this.config.ftp.password,
+      port: this.config.ftp.port,
+      parallel: 5,
+      reload: true,
+      secure: this.config.ftp.sftp,
+      log: this.gutil.log
+    })
+    //  validates if parent folder path exists
+    if (this.fs.existsSync(globs[0].substring(0, globs[0].lastIndexOf('/')))) {
+      // turn off buffering in gulp.src for best performance
+      return this.pump([
+        this.gulp.src(globs, { cwd: '/', buffer: false }),
+        conn.newerOrDifferentSize(dest),
+        conn.dest(dest),
+        this.notify({
+          message: 'Finished deployment.',
+          onLast: true
+        })
+      ])
+    } else {
+      this.gutil.log(this.gutil.colors.red('[Error]'), "Dist file doesn't exists, please verify your paths & files -> " +
+        globs[0])
+      return null
+    }
+  }
+  this.uploadStyles = function () {
+    var base = this.config.ftp.base
+    var rootDist = this.appRoot.path + this.config.app.paths.dist
+    var globs = [rootDist + this.config.app.watch.styles]
+    push(globs, base, true)
+  }
+  this.uploadScripts = function () {
+    var base = this.config.ftp.base
+    var rootDist = this.appRoot.path + this.config.app.paths.dist
+    var globs = [rootDist + this.config.app.watch.scripts]
+    push(globs, base, false)
+  }
+  this.clean = function (path) {
+    this.del(path)
+  }
+}
