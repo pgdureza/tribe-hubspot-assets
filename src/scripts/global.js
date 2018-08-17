670dd61148398867fc74d233b19ab2526f71a4ef
Patrick Dureza <pgdureza@gmail.com>
2018-08-17 23:11:25 +0800

    creator page

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
   });
   
   // animated counter
-  $('.animated_counter').each(function () {
+  $('.animated_counter .figure').each(function () {
     var el = this;
     $(el).appear(function(){
       $(el).prop('Counter',0).animate({
@@ -19,7 +19,8 @@ $(document).ready(function(){
           easing: 'swing',
           step: function (now) {
               // .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") used to add commas
-              $(this).text(Math.ceil(now).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+              var newLine = Math.ceil(parseInt(now)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              $(this).text(newLine);
           }
       });
     })
@@ -57,11 +58,52 @@ $(document).ready(function(){
       });
     }
     $(document).on({
-      scroll: function () {
+      scroll: $.debounce(1, function () {
         parallaxImg();
-      }, ready: function () {
+      }), ready: $.debounce(1, function () {
         parallaxImg();
+      })
+    });
+  });
+
+  $(".parallax-accent").each(function(){
+    var img = $(this);
+    var imgParent = $(this).parent();
+    function parallaxImg () {
+      if ($(window).width() < 900){
+        img.css({
+          transform: 'translate(0,0)'
+        });
+        return false;
       }
+      var speed = -0.3;
+      var imgY = imgParent.offset().top;
+      var winY = $(this).scrollTop();
+      var winH = $(this).height();
+      var parentH = imgParent.innerHeight();
+
+      // The next pixel to show on screen      
+      var winBottom = winY + winH;
+
+      // If block is shown on screen
+      if (winBottom > imgY && winY < imgY + parentH) {
+        // Number of pixels shown after block appear
+        var imgBottom = ((winBottom - imgY) * speed);
+        // Max number of pixels until block disappear
+        var imgTop = winH + parentH;
+        // Porcentage between start showing until disappearing
+        var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+      }
+      img.css({
+        top: (imgPercent * 1.8 - 87) + '%'
+      });
+    }
+    $(document).on({
+      scroll: $.debounce(1, function () {
+        parallaxImg();
+      }), ready: $.debounce(1, function () {
+        parallaxImg();
+      })
     });
   });
 
