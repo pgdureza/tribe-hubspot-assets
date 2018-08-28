db4c2cf59bca48ef94e9fe899d1d23edff072058
Patrick Dureza <pgdureza@gmail.com>
2018-08-28 17:46:12 +0800

    fixed feedback issues

--- src/js/global/globalEvents.js
+++ src/js/global/globalEvents.js
@@ -12,5 +12,6 @@ $(document).ready(function(){
 
 $(window).load(function(){
     // appends newsletter arrow
-    $(".newsletter-wrapper .hs_email .input").append('<submit ><span>SUBSCRIBE</span><span class="arrow-button"> </span></submit>');
+    var arrowImage = '<img src="https://cdn2.hubspot.net/hubfs/1609563/TRIBE%20Website%202018%20Assets/Global%20Assets/Right-1.svg" />';
+    $(".newsletter-wrapper .hs_email .input").append('<submit ><span>SUBSCRIBE</span><span class="arrow-button"> ' + arrowImage + ' </span></submit>');
 });
--- src/js/modules/animated-counter.js
+++ src/js/modules/animated-counter.js
@@ -1,27 +1,46 @@
 // animated counter
-$('.animated_counter .figure').each(function () {
+$('.animated-counter').each(function () {
   var el = this;
-  var startingPoint = $(el).data('starts-at') || $(el).text();
-  var value = $(el).data("value");
-  var slowingPoint = $(el).data('slows-at');
+  var startingPoint = $(el).find('.counter').data("value");
+  var suffix = $(el).find('.counter').data("suffix") || "";
 
-  function animateCounter(start, end, duration, easing){
-    $(el).appear(function(){
-      $(el).prop('Counter', start).animate({
-        Counter: end
-      }, {
-          duration: duration,
-          easing: easing,
-          step: function (now) {
-              // .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") used to add commas
-              var newLine = Math.ceil(parseInt(now)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-              $(this).text(newLine);
-          }
-      });
-    })
+  function triggerAnimation(counter, transitionSpeed){
+    $(el).find(".mask-skew.current").addClass('animating');
+    $(el).find(".mask-skew.next").addClass('animating');
+    setTimeout(function(){
+      // remove animation related classes and reset the 'next' mask to be current
+      $(el).find(".mask-skew.current").remove();
+      $(el).find(".mask-skew.next").removeClass('next').removeClass('animating').addClass('current');
+      createNextElement(counter, suffix, "next");
+      // generate new 'next' mask
+    }, transitionSpeed)
   }
 
-  animateCounter(startingPoint, slowingPoint, 4000, 'linear');
-  animateCounter(slowingPoint, value, 20000, 'easeOutCubic');
-  
+  function createNextElement(counter, suffix, className){
+    var content = Math.ceil(parseInt(counter)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + suffix;
+    $(el).find(".mask-track").append('<div class="mask-skew ' + className + '"><div class="mask">' + content + '</div></div>');
+  }
+
+  function animateCounter(start, end, tickSpeed, transitionSpeed){
+    var counter = start;
+    // generate initial "current" and 'next' elements
+    $(el).find(".mask-skew.current").remove(); // remove initial blank placeholder
+    createNextElement(counter, suffix, "current")
+    counter++;
+    createNextElement(counter, suffix, "next")
+    var tickInterval = setInterval(function(){
+      counter++;
+      triggerAnimation(counter, transitionSpeed)
+      if (counter > end){
+        clearInterval(tickInterval);
+      }
+    }, tickSpeed)
+  }
+
+  $(el).appear(function(){
+    animateCounter((startingPoint - 10), startingPoint, 1000, 500);
+    console.log('appeared');
+  });
+
 }); 
+
