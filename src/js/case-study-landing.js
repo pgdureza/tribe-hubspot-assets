f08055a6c8b334ba6e19aa3677f85905f18a59da
Patrick Dureza <pgdureza@gmail.com>
2018-09-07 18:35:30 +0800

    added filterable cards and added filter states

--- src/js/case-study-landing.js
+++ src/js/case-study-landing.js
@@ -5,5 +5,6 @@ $(document).ready(function(){
   //=include modules/case-study-details.js
   //=include modules/case-study-details-modal.js
   //=include modules/styled-dropdown.js
-
+  //=include= modules/case-study-filters.js
+  
 });
--- src/js/modules/styled-dropdown.js
+++ src/js/modules/styled-dropdown.js
@@ -1,29 +1,49 @@
 // generate the styled dropdown clone
 $(".styled-dropdown").each(function(){
 
+  var isMultiselect = $(this).hasClass('multi-select');
+
   // create the menu items
   var $values = $("<div class='values'>");
   var selected;
-  $(this).find("select option").each(function(){
-    var $option = $(this);
-    var classes = "value ";
-    if ($option.prop('selected')){
-      classes += "selected ";
-      selected = $option.text();
-    }
-
-    if ($option.prop('disabled')){
-      classes += "disabled ";
-    }
-
-    $values.append("<div class='" + classes + "' data-value='" + $option.val() + "'><span>" + $option.text() + "</span></div>");
-  });
+  if (isMultiselect){
+    $(this).find(".checkbox").each(function(){
+      var $option = $(this);
+      var classes = "value ";
+      if ($option.find('input').prop('checked')){
+        classes += "selected ";
+        selected = $option.text();
+      }
+  
+      if ($option.prop('disabled')){
+        classes += "disabled ";
+      }
+  
+      $values.append("<div class='" + classes + "' data-value='" + $option.find('input').val() + "'><span>" + $option.find('label').text() + "</span></div>");
+    });
+  } else {
+    $(this).find("select option").each(function(){
+      var $option = $(this);
+      var classes = "value ";
+      if ($option.prop('selected')){
+        classes += "selected ";
+        selected = $option.text();
+      }
+  
+      if ($option.prop('disabled')){
+        classes += "disabled ";
+      }
+  
+      $values.append("<div class='" + classes + "' data-value='" + $option.val() + "'><span>" + $option.text() + "</span></div>");
+    });
+  }
+  
 
   $(this).append("<div class='selected-value'><span>"+selected+"</span></div>");
 
   // append menu items to dropdown container
   $(this).append($values);
-})
+});
 
 
 // clicking on the dropdown opens the menu
@@ -31,9 +51,12 @@ $(".styled-dropdown").on('click', function(e){
   e.preventDefault();
   var $container = $(this);
   if ($container.hasClass("active")){
-    // close all
-    $(".styled-dropdown").removeClass('active');
+    // close selected
+    $container.removeClass('active');
     $container.find(".values").slideUp(300);
+    if ($container.hasClass('multi-select')){
+      $container.find('input').trigger('change');
+    }
   } else {
     $(".styled-dropdown").removeClass('active');
     $(".styled-dropdown").find(".values").slideUp(300);
@@ -52,13 +75,7 @@ $(document).on('click', function(e){
 });
 
 // clicking on the values triggers a change in the original dropdown
-$(".styled-dropdown .values .value").on('click', function(){
-  // TODO add handling for multiselect
-
-  // disable click on disabled
-  if ($(this).hasClass('disabled')){
-    return false;
-  }
+$(".styled-dropdown:not(.multi-select) .values .value").on('click', function(){
 
   var $option = $(this);
   var $container = $option.closest(".styled-dropdown");
@@ -77,4 +94,44 @@ $(".styled-dropdown .values .value").on('click', function(){
   $container.find('.values').slideUp(300);
 
   return false;
+});
+
+
+// on click, check the hidden checkbox
+$(".styled-dropdown.multi-select .values .value").on('click', function(){
+
+  var $option = $(this);
+  var $container = $option.closest(".styled-dropdown");
+
+  // clicking on all selected clears the other selection
+  if (!$(this).data('value')){
+    $container.find('.selected').removeClass('selected');
+    $container.find('input[type="checkbox"]').prop('checked', false);
+    $option.addClass('selected');
+  } else { // any other option was selected
+    $container.find('[data-value=""]').removeClass('selected');
+    $option.toggleClass('selected');
+    $container.find("input[value='" + $option.data('value') + "']").prop('checked', $option.hasClass('selected'));
+  }
+  
+  // if no items remaining after selecting, reselect 'all categories'
+  if ($container.find('.value.selected').length <= 0){
+    $container.find('[data-value=""]').addClass('selected');
+  }
+
+  // update displayed text
+  if ($container.find(".value.selected").length < 2){
+    $container.find(".selected-value").text($container.find(".value.selected").text());
+  } else {
+    $container.find(".selected-value").text($container.find(".value.selected").length + " selected");
+  }
+
+  return false;
+});
+
+// trigger multi select change
+$(".multi-select").on('blur', function(){
+	if ($(this).hasClass('active')){
+		$(".multi-select input").trigger('change');
+	}
 });
